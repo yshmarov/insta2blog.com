d9092ff7dc412ad9cb3c90e479cd13a32b2ca1ce
yshmarov <yshmarov@gmail.com>
2022-10-19 11:26:38 +0200

    add some future plans

--- app/models/insta_user.rb
+++ app/models/insta_user.rb
@@ -2,4 +2,5 @@ class InstaUser < ApplicationRecord
   has_many :insta_posts, dependent: :destroy
   validates :username, presence: true, uniqueness: true
   validates :remote_id, presence: true, uniqueness: true
+  # enum account_types: { BUSINESS, MEDIA_CREATOR, PERSONAL }
 end
