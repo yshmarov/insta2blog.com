35a1f25f788b89d89f43e87ee87d13a2f45cc8af
yshmarov <yshmarov@gmail.com>
2022-10-22 22:56:07 +0200

    button to refresh posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= InstaUser.find(session[:insta_user_id]) if session[:insta_user_id]
+  rescue ActiveRecord::RecordNotFound
+    nil
   end
   helper_method :current_user
 end
