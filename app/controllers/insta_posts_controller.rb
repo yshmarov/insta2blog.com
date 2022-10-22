9d152c046ae796ff25d8a8bfad96553f3227da21
yshmarov <yshmarov@gmail.com>
2022-10-22 14:19:14 +0200

    lint

--- app/controllers/insta_posts_controller.rb
+++ app/controllers/insta_posts_controller.rb
@@ -2,7 +2,9 @@ class InstaPostsController < ApplicationController
   before_action :set_user, only: %i[index]
 
   def index
+    # should not be called here. should be some button to "import" that would trigger a job.
     InstaMediaService.new(@insta_user).call
+
     @posts = @insta_user.insta_posts.order(timestamp: :desc)
   end
 
--- test/integration/insta_posts_test.rb
+++ test/integration/insta_posts_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class InstaPostsTest < ActionDispatch::IntegrationTest
   def setup
