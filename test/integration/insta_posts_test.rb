c60759bbc9a99ae554e5b0ee6e23431624f46a24
yshmarov <yshmarov@gmail.com>
2022-10-22 14:13:32 +0200

    clean up, move specs

---
+++ test/integration/insta_posts_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class InstaPostsTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = InstaUser.create(username: 'yaro_the_slav', remote_id: '123')
+    @user.insta_access_tokens.create
+  end
+
+  test 'posts' do
+    skip # will stub faraday requests later
+    get insta_user_posts_url(@user)
+    assert_response :success
+  end
+end
