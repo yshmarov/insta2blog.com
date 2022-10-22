df19e8d6e2a5bc6af4ec149d56536e0f9831d78a
Yaroslav Shmarov <yshmarov@gmail.com>
2022-10-22 11:51:55 +0200

    try to fix undefined instagram_callback_url (#11)

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,20 +1,12 @@
 class InstagramController < ApplicationController
   CLIENT_ID = Rails.application.credentials.dig(:instagram, :client_id).to_s
   CLIENT_SECRET = Rails.application.credentials.dig(:instagram, :client_secret).to_s
-  REDIRECT_URI = if Rails.env.production?
-                   instagram_callback_url
-                 else
-                   # staging
-                   # 'localhost:3000/instagram/callback/'
-                   # 'https://insta2blog.com/instagram/callback'
-                   'https://insta2site.herokuapp.com/'
-                 end
 
   # GET /instagram/authorize
   def authorize
     # Link to log in with instagram.
     authorize_url = 'https://api.instagram.com/oauth/authorize'
-    redirect_to "#{authorize_url}?client_id=#{CLIENT_ID}&redirect_uri=#{REDIRECT_URI}&scope=user_profile,user_media&response_type=code",
+    redirect_to "#{authorize_url}?client_id=#{CLIENT_ID}&redirect_uri=#{redirect_uri}&scope=user_profile,user_media&response_type=code",
                 allow_other_host: true
   end
 
@@ -24,11 +16,25 @@ class InstagramController < ApplicationController
     code = params[:code]
     return head :bad_request unless code
 
-    insta_user_id = InstaAuthService.new(code, REDIRECT_URI).call
-    # return head :bad_request unless insta_user
+    insta_user_id = InstaAuthService.new(code, redirect_uri).call
+    return head :bad_request unless insta_user
 
     session[:insta_user_id] = insta_user_id
 
     redirect_to insta_user_path(insta_user_id)
+  end
+
+  private
+
+  def redirect_uri
+    if Rails.env.production?
+      # Rails.application.routes.url_helpers.instagram_callback_url
+      instagram_callback_url
+    else
+      # staging
+      # 'localhost:3000/instagram/callback/'
+      # 'https://insta2blog.com/instagram/callback'
+      'https://insta2site.herokuapp.com/'
+    end
   end
 end
