07ae399f21a71cf7927463e2f613b45517de2200
Yaroslav Shmarov <yshmarov@gmail.com>
2022-10-24 01:17:59 +0200

    35 instapost click to copy url share fuctionality [IMPROVED] (#39)
    * feat/copy post url to clipboard
    
    * enhance controller to display copied animation
    
    * Update clipboard_controller.js

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,11 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="clipboard"
 export default class extends Controller {
-  static targets = [ "source" ]
+  static targets = ["source", "trigger"]
 
-  copy() {
+  copy(e) {
+    e.preventDefault()
     navigator.clipboard.writeText(this.sourceTarget.value)
+ 
     this.sourceTarget.focus()
+    var triggerElement = this.triggerTarget
+    var initialHTML = triggerElement.innerHTML
+    triggerElement.innerHTML = "<span>Copied</span>"
+    setTimeout(() => {
+      triggerElement.innerHTML = initialHTML
+    }, 2000)
   }
 }
