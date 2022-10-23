16f457712a34d1cc356a2e6c11b6f8acf209c095
yshmarov <yshmarov@gmail.com>
2022-10-24 01:13:54 +0200

    enhance controller to display copied animation

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,11 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
-export default class extends Controller {
-  static targets = [ "source" ]
+// export default class extends Controller {
+//   static targets = [ "source" ]
 
-  copy() {
+//   copy() {
+//     navigator.clipboard.writeText(this.sourceTarget.value)
+//     this.sourceTarget.focus()
+//   }
+// }
+
+export default class extends Controller {
+  static targets = ["source", "trigger"]
+
+  copy(e) {
+    e.preventDefault()
     navigator.clipboard.writeText(this.sourceTarget.value)
     this.sourceTarget.focus()
+    var triggerElement = this.triggerTarget
+    var initialHTML = triggerElement.innerHTML
+    triggerElement.innerHTML = "<span>Copied</span>"
+    setTimeout(() => {
+      triggerElement.innerHTML = initialHTML
+    }, 2000)
   }
 }
