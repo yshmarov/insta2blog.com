d028b65d74d278802b097c3c8ff0b00286503188
yshmarov <yshmarov@gmail.com>
2022-10-24 01:15:02 +0200

    Update clipboard_controller.js

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,21 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="clipboard"
-// export default class extends Controller {
-//   static targets = [ "source" ]
-
-//   copy() {
-//     navigator.clipboard.writeText(this.sourceTarget.value)
-//     this.sourceTarget.focus()
-//   }
-// }
-
 export default class extends Controller {
   static targets = ["source", "trigger"]
 
   copy(e) {
     e.preventDefault()
     navigator.clipboard.writeText(this.sourceTarget.value)
+ 
     this.sourceTarget.focus()
     var triggerElement = this.triggerTarget
     var initialHTML = triggerElement.innerHTML
