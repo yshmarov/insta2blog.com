bb8d079ecddc060649947f7aa34f4ad2a6c9361e
yshmarov <yshmarov@gmail.com>
2022-10-25 00:17:30 +0200

    find hashtags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def with_hashtags(text)
+    return nil if text.blank?
+
+    text.gsub(/\S*#(\[[^\]]+\]|\S+)/, '<a href="http://localhost:3000/u/za-yuliia/p?caption=\1" class="hashtag">#\1</a>')
+  end
 end
