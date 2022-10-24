23d003e7fda85e0197c5b38a7acb2e5525f07ca7
yshmarov <yshmarov@gmail.com>
2022-10-24 21:32:06 +0200

    move insta_me logic to separate service

--- app/services/insta_me_service.rb
+++ app/services/insta_me_service.rb
@@ -1,20 +1,20 @@
-# insta_user = InstaUser.first
-# updated_insta_user = InstaMeService.new(insta_user).call
+# insta_user = InstaMeService.new(insta_user).call
 class InstaMeService
-  attr_reader :insta_user, :insta_access_token
+  attr_reader :long_lived_access_token
 
-  def initialize(insta_user)
-    @insta_user = insta_user
-    @insta_access_token = insta_user.insta_access_tokens.last
+  def initialize(long_lived_access_token)
+    @long_lived_access_token = long_lived_access_token
   end
 
   def call
     insta_user_data = ask_user_profile
+    insta_user = InstaUser.find_or_initialize_by(username: insta_user_data['username'])
     insta_user.update(
-      username: insta_user_data['username'],
+      remote_id: insta_user_data['id'],
       account_type: insta_user_data['account_type'].downcase,
       media_count: insta_user_data['media_count']
     )
+    insta_user
   end
 
   private
@@ -22,7 +22,7 @@ class InstaMeService
   def ask_user_profile
     response = Faraday.get('https://graph.instagram.com/me') do |req|
       req.headers = headers,
-                    req.params = user_params(insta_access_token.access_token)
+                    req.params = user_params(long_lived_access_token)
     end
     JSON.parse(response.body)
     # {"id"=>"5973192396032263", "username"=>"yaro_the_slav", "account_type"=>"PERSONAL", "media_count"=>305}
