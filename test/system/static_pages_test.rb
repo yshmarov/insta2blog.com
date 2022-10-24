4609864d1350e68a080b10ad351197a2b3406a69
Yaroslav Shmarov <yshmarov@gmail.com>
2022-10-25 01:28:13 +0200

    scan post for hashtags, highlight them and generate links (#49)
    * find hashtags
    
    * parse hashtags for posts caption
    
    * add some system specs
    
    * Create insta_posts_helper_test.rb

--- test/system/static_pages_test.rb
+++ test/system/static_pages_test.rb
@@ -1,7 +1,7 @@
 require 'application_system_test_case'
 
 class StaticPagesTest < ApplicationSystemTestCase
-  test 'visiting the homepage' do
+  test 'static_pages#landing_page' do
     visit root_url
 
     assert_text 'Connect your Instagram'
