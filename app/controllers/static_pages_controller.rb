fd847232e1004b0b1bdb98e01ef8ec517e53062e
yshmarov <yshmarov@gmail.com>
2022-10-24 20:25:40 +0200

    updating landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
   before_action :seo_tags, only: %i[terms privacy pricing]
-  def landing_page; end
+  def landing_page
+    count = InstaUser.count
+    random_offset = rand(count)
+    @random_user = InstaUser.offset(random_offset).first
+  end
+
   def terms; end
   def privacy; end
   def pricing; end
