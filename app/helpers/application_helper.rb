29d889d206b307b71b4e6ecc980d1fa75c8c78af
yshmarov <yshmarov@gmail.com>
2022-10-25 01:17:11 +0200

    add some system specs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
 module ApplicationHelper
-  def with_hashtags(post)
-    return nil if post.caption.blank?
-
-    body = post.caption
-    hashtags = body.scan(/#\w+/)
-    hashtags.flatten.each do |hashtag|
-      hashtag_link =
-        link_to hashtag,
-                insta_user_posts_path(post.insta_user, caption: hashtag),
-                data: { turbo: false },
-                class: 'hashtag'
-      body.gsub!(hashtag, hashtag_link)
-    end
-    body
-  end
 end
