2bb7013bd7bee4e72c5ad2c5d4fd36ef247a8b47
yshmarov <yshmarov@gmail.com>
2022-10-24 14:45:21 +0200

    work on adding SEO tags to posts

--- app/controllers/insta_posts_controller.rb
+++ app/controllers/insta_posts_controller.rb
@@ -2,6 +2,7 @@ class InstaPostsController < ApplicationController
   before_action :set_user
 
   def index
+    index_seo_tags
     posts = @insta_user.insta_posts.order(timestamp: :desc)
     @posts = if params[:caption].present?
                posts.where('caption ilike ?', "%#{params[:caption]}%")
@@ -18,17 +19,30 @@ class InstaPostsController < ApplicationController
 
   def show
     @post = @insta_user.insta_posts.find(params[:post_id])
+    show_seo_tags
   end
 
   private
 
   def set_user
     @insta_user = InstaUser.find(params[:id])
-    seo_tags
   end
 
-  def seo_tags
-    set_meta_tags title: @insta_user.username,
-                  description: "#{@insta_user.username} blog website"
+  def index_seo_tags
+    set_meta_tags title: ['Feed', @insta_user.username],
+                  description: "#{@insta_user.username} feed with all posts",
+                  keywords: [@insta_user.username, 'blog', 'instablog', 'instagram'],
+                  author: @insta_user.username
+  end
+
+  def show_seo_tags
+    set_meta_tags title: [@post.meta_title, @insta_user.username],
+                  description: @post.meta_description,
+                  keywords: @post.meta_keywords,
+                  image_src: @post.meta_image,
+                  author: @insta_user.username,
+                  article: @post.meta_article,
+                  twitter: @post.meta_twitter,
+                  og: @post.meta_og
   end
 end
--- app/models/insta_post.rb
+++ app/models/insta_post.rb
@@ -5,4 +5,60 @@ class InstaPost < ApplicationRecord
 
   extend FriendlyId
   friendly_id :remote_id, use: %i[finders]
+
+  def meta_title
+    # first sentence OR text before first line break
+    word_count = 10
+    title = caption.split[0...word_count].join(' ')
+    title.truncate(70)
+  end
+
+  def meta_description
+    # sentences 2,3,4?
+    caption.truncate(300)
+  end
+
+  def meta_keywords
+    [insta_user.username, 'blog', 'instablog', 'instagram']
+    # TODO: caption.detected_hashtags
+  end
+
+  def meta_image
+    return thumbnail_url if video?
+
+    media_url
+  end
+
+  def meta_article
+    {
+      published_time: timestamp,
+      modified_time: updated_at,
+      section: 'Article Section',
+      tag: 'Article Tag'
+    }
+  end
+
+  def meta_twitter
+    {
+      card: 'summary',
+      site: '@username'
+    }
+  end
+
+  def meta_og
+    {
+      title: meta_title,
+      type: 'video.movie',
+      url: 'http://www.imdb.com/title/tt0117500/',
+      image: meta_image,
+      video: {
+        director: 'http://www.imdb.com/name/nm0000881/',
+        writer: ['http://www.imdb.com/name/nm0918711/', 'http://www.imdb.com/name/nm0177018/']
+      }
+    }
+  end
+
+  def meta_icon
+    '/images/icons/itos-logo.png'
+  end
 end
