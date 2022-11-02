9d87d7f435232d9842cacf86324b222fc967d02a
Yaroslav Shmarov <yshmarov@gmail.com>
2022-11-02 18:51:32 +0100

    add last_import_at:datetime to InstaUser (#59)
    * add last_import_at:datetime to InstaUser
    
    * Update show.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_215628) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_173539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_215628) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "insta_posts_count", default: 0, null: false
+    t.datetime "last_import_at"
     t.index ["remote_id"], name: "index_insta_users_on_remote_id", unique: true
     t.index ["slug"], name: "index_insta_users_on_slug", unique: true
     t.index ["username"], name: "index_insta_users_on_username", unique: true
