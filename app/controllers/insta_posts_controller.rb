297adedfef5125708f7766e98b8d983a8089b054
yshmarov <yshmarov@gmail.com>
2022-11-05 11:22:15 +0100

    get random, not latest user posts

--- app/controllers/insta_posts_controller.rb
+++ app/controllers/insta_posts_controller.rb
@@ -20,7 +20,7 @@ class InstaPostsController < ApplicationController
 
   def show
     @post = @insta_user.insta_posts.find(params[:post_id])
-    @posts = @insta_user.insta_posts.without(@post).limit(6)
+    @posts = @insta_user.insta_posts.without(@post).order('RANDOM()').limit(6)
   end
 
   private
