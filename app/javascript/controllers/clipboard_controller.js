462df186585b4fd70d508ffbef5e5124cf671fc5
Yaroslav Shmarov <yshmarov@gmail.com>
2022-11-06 17:10:51 +0100

    Style "post" card (#69)
    * style username links
    
    * add alt to logo
    
    * Update clipboard_controller.js
    
    * Update _insta_post.html.erb
    
    * style post footer
    
    * add conditional clicked value to clipboard controller

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["source", "trigger"]
+  static values = { clicked: String }
 
   copy(e) {
     e.preventDefault()
@@ -10,7 +11,7 @@ export default class extends Controller {
     this.sourceTarget.focus()
     var triggerElement = this.triggerTarget
     var initialHTML = triggerElement.innerHTML
-    triggerElement.innerHTML = "<span style='color:grey;'>Copied</span>"
+    triggerElement.innerHTML = this.clickedValue
     setTimeout(() => {
       triggerElement.innerHTML = initialHTML
       this.sourceTarget.blur()
