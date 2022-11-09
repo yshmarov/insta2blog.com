2fec4b0dcae4ff7df561ad715f0634b53fe173e6
yshmarov <yshmarov@gmail.com>
2022-11-09 19:26:38 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_many :insta_users, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 
   passwordless_with :email
 
