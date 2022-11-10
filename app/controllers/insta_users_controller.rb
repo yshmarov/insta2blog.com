12ff51023532d8f772ab8aec91384b7dd8b49fd3
yshmarov <yshmarov@gmail.com>
2022-11-11 00:59:24 +0100

    refresh user media count on load

--- app/controllers/insta_users_controller.rb
+++ app/controllers/insta_users_controller.rb
@@ -1,6 +1,6 @@
 class InstaUsersController < ApplicationController
-  before_action :set_user, only: %i[show import destroy]
-  before_action :require_user!, only: %i[import destroy]
+  before_action :set_user, only: %i[show import destroy media_count]
+  before_action :require_user!, only: %i[import destroy media_count]
 
   # GET /u
   def index
@@ -21,6 +21,24 @@ class InstaUsersController < ApplicationController
     # TODO: should trigger a job
     InstaMediaService.new(@insta_user).call
     redirect_to insta_user_posts_path(@insta_user), notice: t('.success')
+  end
+
+  # GET /u/:id/media_count
+  def media_count
+    # TODO: should be last_me_at
+    media_count = if @insta_user.last_import_at < 15.minutes.ago
+                    insta_access_token = @insta_user.insta_access_tokens.first
+                    insta_user = InstaMeService.new(insta_access_token.access_token).call
+                    insta_user.media_count
+                  else
+                    @insta_user.media_count
+                  end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("#{@insta_user.id}_media_count", html: media_count)
+      end
+    end
   end
 
   # DELETE /u/:id
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'u/:id', to: 'insta_users#show', as: :insta_user
   delete 'u/:id', to: 'insta_users#destroy', as: :delete_insta_user
   post 'u/:id/import', to: 'insta_users#import', as: :import_insta_user
+  get 'u/:id/media_count', to: 'insta_users#media_count', as: :media_count_insta_user
 
   get 'u/:user_id/p', to: 'insta_posts#index', as: :insta_user_posts
   get 'u/:user_id/p/:id', to: 'insta_posts#show', as: :insta_user_post
