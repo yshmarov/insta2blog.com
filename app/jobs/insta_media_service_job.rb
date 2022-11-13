0509b7dab40e2f3e887674fc267bdae2d98859fb
yshmarov <yshmarov@gmail.com>
2022-11-13 17:33:12 +0100

    ProcessCaptionServiceJobTest

--- app/jobs/insta_media_service_job.rb
+++ app/jobs/insta_media_service_job.rb
@@ -2,7 +2,6 @@ class InstaMediaServiceJob < ApplicationJob
   queue_as :default
 
   def perform(insta_user)
-    puts 'InstaMediaService...'
     InstaMediaService.new(insta_user).call
   end
 end
--- test/jobs/process_caption_service_job_test.rb
+++ test/jobs/process_caption_service_job_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ProcessCaptionServiceJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = InstaUser.create(username: 'za.yuliia', remote_id: SecureRandom.random_number(9999))
+    @post = InstaPost.create(insta_user: @user,
+                             remote_id: SecureRandom.random_number(9999),
+                             timestamp: Time.zone.now,
+                             caption: 'Post with a #hashtag')
+  end
+
+  test 'the truth' do
+    assert_nil @post.processed_caption
+
+    ProcessCaptionServiceJob.perform_now(@post)
+
+    assert_equal processed_caption_text.squish, @post.processed_caption
+  end
+
+  private
+
+  def processed_caption_text
+    <<~TEXT
+      Post with a <a data-turbo="false" class="hashtag font-semibold" href="/u/za-yuliia/p?caption=%23hashtag&amp;onlypath=true">#hashtag</a>
+    TEXT
+  end
 end
