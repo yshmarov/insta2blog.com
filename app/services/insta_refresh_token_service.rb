f77674e7c68c35edc6264334b90e02ffb1d9936a
yshmarov <yshmarov@gmail.com>
2022-11-25 12:20:04 +0100

    Update insta_refresh_token_service.rb

--- app/services/insta_refresh_token_service.rb
+++ app/services/insta_refresh_token_service.rb
@@ -1,36 +1,31 @@
-# can I still use previous token to make an API call?
+# Refresh expiring bearer token for accessing the API. 60 days valid.
+# API reference:
 # https://developers.facebook.com/docs/instagram-basic-display-api/guides/long-lived-access-tokens#long-lived-access-tokens
 
-# insta_access_token = InstaAccessToken.last
-# new_long_lived_access_token = InstaRefreshTokenService.new(insta_access_token).call
+# insta_access_token_id = InstaAccessToken.last.id
+# InstaRefreshTokenService.new(insta_access_token_id).call
 class InstaRefreshTokenService
   attr_reader :insta_access_token, :insta_user
 
-  def initialize(insta_access_token)
-    @insta_access_token = insta_access_token
+  def initialize(insta_access_token_id)
+    @insta_access_token = InstaAccessToken.find(insta_access_token_id)
     @insta_user = insta_access_token.insta_user
   end
 
   def call
-    # get a new token
     response = Faraday.get('https://graph.instagram.com/refresh_access_token') do |req|
       req.headers = headers,
                     req.params = params(insta_access_token.access_token)
     end
-    # {"access_token"=> "UXE5RDNR", "token_type"=>"bearer", "expires_in"=>5143192}
-    JSON.parse(response.body)
-    # if success
-    #   new_insta_access_token = InstaAccessToken.create(data.except('token_type'))
-    #   new_insta_access_token.update(insta_user_id: insta_user.id)
-    #   insta_access_token.update(valid: false)
-    #   insta_access_token.destroy
-    # end
+    return unless response.status == 200
+
+    data = JSON.parse(response.body)
+    insta_access_token.update(access_token: data['access_token'],
+                              expires_in: data['expires_in'],
+                              expires_at: Time.zone.now + data['expires_in'])
   end
 
   private
-
-  def check_current_token
-  end
 
   def headers
     { Accept: 'application/json' }
