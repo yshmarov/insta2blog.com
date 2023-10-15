d16d56788016feed0923bb74e6c2e14d5b583d01
Yaro Shm <yshmarov@gmail.com>
2023-10-15 14:13:48 +0300

    Move Insta carousel service into insta carousel job (#113)
    * InstaCarouselService into InstaCarouselJob
    
    * test schedule job

--- test/jobs/insta_carousel_job_test.rb
+++ test/jobs/insta_carousel_job_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class InstaCarouselJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @insta_user = InstaUser.create(username: 'za.yuliia', remote_id: SecureRandom.random_number(9999))
+    @insta_post = InstaPost.create(insta_user: @insta_user,
+                                   media_type: :carousel_album,
+                                   remote_id: SecureRandom.random_number(9999))
+  end
+
+  test 'should perform job' do
+    assert_enqueued_jobs 0
+    InstaCarouselJob.perform_later(@insta_post)
+    assert_enqueued_jobs 1
+  end
 end
