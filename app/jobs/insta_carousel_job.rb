57cb25ed2aa5b69b5b3920b71ad6d1d46e57ade4
yshmarov <yshmarov@gmail.com>
2023-10-15 13:02:47 +0200

    InstaCarouselService into InstaCarouselJob

--- app/jobs/insta_carousel_job.rb
+++ app/jobs/insta_carousel_job.rb
@@ -1,7 +1,62 @@
+# GET CAROUSEL_ALBUM children for an InstaPost.
+# API reference:
+# https://developers.facebook.com/docs/instagram-basic-display-api/reference/media/children#reading
+
+# insta_post = InstaPost.carousel_album.first
+# InstaCarouselJob.perform_now(insta_post)
 class InstaCarouselJob < ApplicationJob
   queue_as :default
 
   def perform(insta_post)
-    InstaCarouselService.new(insta_post).call
+    @insta_post = insta_post
+    return unless @insta_post.carousel_album?
+
+    @insta_access_token = @insta_post.insta_user.insta_access_tokens.active.last
+    ask_media_children(@insta_post.remote_id, @insta_access_token.access_token)
+  end
+
+  private
+
+  def ask_media_children(id, access_token)
+    response = Faraday.get("https://graph.instagram.com/#{id}/children") do |request|
+      request.headers = headers,
+                        request.params = { access_token: }
+    end
+
+    page = JSON.parse(response.body)
+    items = page['data']
+    items.each do |item|
+      id = item['id']
+      ask_media_id(id, access_token)
+    end
+  end
+
+  def ask_media_id(id, access_token)
+    response = Faraday.get("https://graph.instagram.com/#{id}") do |request|
+      request.headers = headers,
+                        request.params = carousel_item_params(access_token)
+    end
+
+    item = JSON.parse(response.body)
+    insta_carousel_item = @insta_post.insta_carousel_items
+                                    .find_or_create_by(remote_id: item['id'].to_i)
+    insta_carousel_item.update(
+      media_type: item['media_type'].downcase,
+      media_url: item['media_url'],
+      permalink: item['permalink'],
+      thumbnail_url: item['thumbnail_url'],
+      timestamp: item['timestamp']
+    )
+  end
+
+  def headers
+    { Accept: 'application/json' }
+  end
+
+  def carousel_item_params(access_token)
+    {
+      fields: 'id,media_type,media_url,permalink,thumbnail_url,timestamp',
+      access_token:
+    }
   end
 end
