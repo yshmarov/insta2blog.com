e9f623cd427df619b73effe06b0a720b311b6439
yshmarov <yshmarov@gmail.com>
2023-11-09 20:59:56 +0100

    linting

--- test/mailers/previews/passwordless_mailer_preview.rb
+++ test/mailers/previews/passwordless_mailer_preview.rb
@@ -1,6 +1,6 @@
 class PasswordlessMailerPreview < ActionMailer::Preview
   def sign_in
-    user = User.build(email: "foo@bar.com")
+    user = User.build(email: 'foo@bar.com')
     session = Passwordless::Session.create!(authenticatable: user)
     Passwordless::Mailer.sign_in(session)
   end
