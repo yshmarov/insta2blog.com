a5252c6608838d46e95a1bb6e3d1a28bd0d15118
Yaro Shm <yshmarov@gmail.com>
2025-01-31 22:06:08 +0100

    importmaps, tailwind, js from rails 8

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,8 +15,8 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/container-queries'),
   ]
 }
