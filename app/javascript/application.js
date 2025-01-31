0a6d92220c526d132bb54193898898fff6d070f7
Yaro Shm <yshmarov@gmail.com>
2025-01-31 21:38:10 +0100

    fontawesome via css, not js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "@fortawesome/fontawesome-free"
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin '@fortawesome/fontawesome-free', to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js'
